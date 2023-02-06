d31197233531197122d5ed920c793319c1a79874
Aslak Sandvik Tangen <Aslak Sandvik Tangen@DESKTOP-RM73LJC>
2023-02-06 14:02:33 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace Brunvoll_utplasseringsprosjekt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(65, 377);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(234, 188);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = global::Brunvoll_utplasseringsprosjekt.Properties.Resources.BC_5_panel_azimuth;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.Location = new System.Drawing.Point(65, 100);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(234, 191);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "s";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.LightGray;
+            this.panel1.Location = new System.Drawing.Point(401, 100);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1239, 1007);
+            this.panel1.TabIndex = 5;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(1696, 100);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(237, 61);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Export";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1945, 1194);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Brunvoll_utplasseringsprosjekt
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            panel1.AllowDrop = true;
+            button1.AllowDrop = true;
+
+            panel1.DragEnter += panel_DragEnter;
+            button1.DragEnter += panel_DragEnter;
+
+            panel1.DragDrop += panel_DragDrop;
+            button1.DragDrop += panel_DragDrop;
+
+            button1.MouseDown += button1_MouseDown;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var test = new Button();
+            test.BackgroundImage = Properties.Resources.BC_5_panel_azimuth;
+            test.BackgroundImageLayout = ImageLayout.Stretch;
+            test.Height = 130;
+            test.Width = 150;
+            this.button1.Controls.Add(test);
+
+            test.DragDrop += panel_DragDrop;
+            test.AllowDrop = true;
+            test.MouseDown += delegate
+            {
+                if (button1.Controls.Contains(test))
+                {
+                    test.DoDragDrop(test, DragDropEffects.Move);
+                }
+                else
+                {
+                    ControlExtension.Draggable(test, true);
+                }
+            };
+
+            
+            
+        }
+
+        private void button1_MouseDown(object sender, MouseEventArgs e)
+        {
+            //button1.DoDragDrop(button1, DragDropEffects.Move);
+        }
+
+        void panel_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.Move;
+        }
+
+        void panel_DragDrop(object sender, DragEventArgs e)
+        {
+            ((Button)e.Data.GetData(typeof(Button))).Parent = (Panel)sender;
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            using (var bmp = new Bitmap(panel1.Width, panel1.Height))
+            {
+                panel1.DrawToBitmap(bmp, new Rectangle(0, 0, bmp.Width, bmp.Height));
+
+                SaveFileDialog sfd = new SaveFileDialog();
+                sfd.Filter = "Images|*.png;*.bmp;*.jpg";
+                ImageFormat format = ImageFormat.Png;
+                if (sfd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    string ext = System.IO.Path.GetExtension(sfd.FileName);
+                    switch (ext)
+                    {
+                        case ".jpg":
+                            format = ImageFormat.Jpeg;
+                            break;
+                        case ".bmp":
+                            format = ImageFormat.Bmp;
+                            break;
+                    }
+                    bmp.Save(sfd.FileName, format);
+                }
+
+                
+            }
+        }
+    }
+}
+ 
